c4016a16c8ba0c48a59772ba1110585ef5cc9bc8
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-10 08:29:21 +0000

    Save

--- Decryption_Substitution.c
+++ Decryption_Substitution.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+char alphatxt[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+char subtxt[]=    "QWERTYUIOPASDFGHJKLZXCVBNM ";
+char N = 27;
+char m;
+char i;
+char a;
+char b;
 
+
+FILE *coded;
+coded = fopen("coded.txt", "r");
+
+FILE *message;
+message = fopen("message.txt", "w");
+    
+    while (feof(coded) == 0){
+        fscanf(coded, "%c", &m);
+    for (i = 0; i < N; ++i){
+        a = alphatxt[i];
+        b = subtxt[i];
+    if (m == b){
+        fprintf(message, "%c", a);
+        printf("%c", a);
+    } 
+       
+}
+
+}
 }
